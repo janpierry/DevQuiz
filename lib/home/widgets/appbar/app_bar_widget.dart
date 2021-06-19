2366036f6250d11fb94fc5f8a609a73ec3822c4a
janpierry <janpierrycoelho@gmail.com>
2021-06-18 23:16:38 -0300

    Finishes the course version of app

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,10 +1,12 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+  AppBarWidget({required this.user})
       : super(
           preferredSize: Size.fromHeight(250),
           child: Container(
@@ -25,7 +27,9 @@ class AppBarWidget extends PreferredSize {
                           style: AppTextStyles.title,
                           children: [
                             TextSpan(
-                                text: "Jan", style: AppTextStyles.titleBold)
+                              text: user.name,
+                              style: AppTextStyles.titleBold,
+                            )
                           ],
                         ),
                       ),
@@ -35,14 +39,17 @@ class AppBarWidget extends PreferredSize {
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://avatars.githubusercontent.com/u/22162290?v=4")),
+                              image: NetworkImage(user.photoUrl)),
                         ),
                       )
                     ],
                   ),
                 ),
-                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget()),
+                Align(
+                    alignment: Alignment(0.0, 1.0),
+                    child: ScoreCardWidget(
+                      percent: user.score / 100,
+                    )),
               ],
             ),
           ),
